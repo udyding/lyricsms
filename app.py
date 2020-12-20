8dd75f49458357924f4e5fbd9c16237d98e2aff7
Udy Ding <udyding@MacBook-Pro.local>
2020-12-20 17:11:01 -0500

    heroku

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
+import os
 from flask import Flask
 from twilio.rest import Client
-
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
+# initialize twilio
+ACCOUNT_SID = os.getenv("ACCOUNT_SID")
+AUTH_TOKEN = os.getenv("AUTH_TOKEN")
+
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
 
 @app.route("/")
-def index():
-    return "Hello, world!"
+def get_lyrics():
+    # message = client.messages.create(
+    #     body="Hello there",
+    #     from_='+16479458787',
+    #     to='+16479838785'
+    # )
+    return "hello, world"
 
 
 if __name__ == '__main__':
