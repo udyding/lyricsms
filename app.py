f01cc4e9f1749e1ab0f45dbb9beeeab45f6bdbbd
Udy Ding <udyding@MacBook-Pro.local>
2020-12-20 16:52:57 -0500

    env

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from twilio.rest import Client
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, world!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
