7bc440f5dcefc81f10f90f57a27e4bf5a2dc99da
Udy Ding <udyding@MacBook-Pro.local>
2020-12-20 17:52:08 -0500

    test reply

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from twilio.rest import Client
 from dotenv import load_dotenv
+from twilio.twiml.messaging_response import MessagingResponse
 load_dotenv()
 
 app = Flask(__name__)
@@ -13,14 +14,21 @@ AUTH_TOKEN = os.getenv("AUTH_TOKEN")
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def get_lyrics():
-    message = client.messages.create(
-        body="Hello there",
-        from_='+16479458787',
-        to='+16479838785'
-    )
-    return "hello, world"
+    # message = client.messages.create(
+    #     body="Hello there",
+    #     from_='+16479458787',
+    #     to='+16479838785'
+    # )
+    # return "hello, world"
+
+    # messaging response
+    resp = MessagingResponse()
+
+    resp.message("HIHI")
+
+    return str(resp)
 
 
 if __name__ == '__main__':
