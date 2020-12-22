87bd1bfe594a810d74e82a75ec553254ee11c991
Udy Ding <udyding@MacBook-Pro.local>
2020-12-22 16:08:45 -0500

    finished main functionality

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+from scrape import scrape_song_info, scrape_top_five
+from scrape import scrape_song_info
+from flask import Flask, request
 from twilio.rest import Client
 from dotenv import load_dotenv
 from twilio.twiml.messaging_response import MessagingResponse
@@ -13,22 +15,79 @@ AUTH_TOKEN = os.getenv("AUTH_TOKEN")
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
+choices = {}
+
 
 @app.route("/", methods=['GET', 'POST'])
 def get_lyrics():
-    # message = client.messages.create(
-    #     body="Hello there",
-    #     from_='+16479458787',
-    #     to='+16479838785'
-    # )
-    # return "hello, world"
+    global choices
+    from_number = request.args.get('From')
+    query = request.args.get('Body')
 
-    # messaging response
     resp = MessagingResponse()
+    message = "\n\n"
 
-    resp.message("HIHI")
+    if query[0] == "?":
+        selection = int(query[1])
+        url = choices.get(selection, None)
+        if not url:
+            message += "Invalid selection."
+        else:
+            song_info = scrape_song_info(choices[selection])
+            message += f"{song_info[1]} - {song_info[0]}\n\n"
+            message += song_info[2]
+    else:
+        top_five = scrape_top_five(query)
+        choices = top_five[1]
+        message += top_five[0]
 
-    return str(resp)
+    print(message)
+    print(len(message))
+    send_message(message, "+16479458787", from_number)
+    # resp.message(message)
+
+    # return str(resp)
+    return "success"
+
+
+def send_message(message, from_=None, to=None):
+    if len(message) < 1400:
+        client.messages.create(
+            body=message,
+            from_=from_,
+            to=to
+        )
+    else:
+        lines = message.split("\n")
+        char_count = 0
+        temp_message = ""
+        for line in lines:
+            if char_count + len(line) >= 1400:
+                try:
+                    client.messages.create(
+                        body=f"\n{temp_message}",
+                        from_=from_,
+                        to=to
+                    )
+                except Exception as e:
+                    print("SEND FAILED")
+                    print(e)
+                    print(temp_message)
+                    print(len(temp_message))
+                char_count = 0
+                temp_message = ""
+
+            char_count += len(line) + 1
+            temp_message += f"\n{line}"
+
+        if temp_message:
+            client.messages.create(
+                body=f"\n{temp_message}",
+                from_=from_,
+                to=to
+            )
+
+    return True
 
 
 if __name__ == '__main__':
---
+++ scrape.py
@@ -0,0 +1,114 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+# get the data
+
+
+def scrape_song_info(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    song_info = soup.title.string.replace(u'\xa0', u' ')
+    artist = song_info.split(" - ")[0]
+    title = song_info.split(" - ")[1].split(" Lyrics")[0]
+
+    lyrics = soup.find('b', text=f'"{title}"'
+                       ).find_next('div').text.split("\n")
+
+    lyrics = '\n'.join(lyrics[2:])
+
+    return artist, title, lyrics  # string, string, string
+
+
+def scrape_top_five(search):
+    url = f"https://search.azlyrics.com/search.php?q={search}"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    song_songs = []
+    song_artists = []
+    song_links = []
+
+    lyrics_songs = []
+    lyrics_artists = []
+    lyrics_links = []
+
+    song_results = soup.find('b', text="Song results:")
+    lyrics_results = soup.find('b', text="Lyrics results:")
+
+    has_song_results = False
+    has_lyrics_results = False
+
+    if song_results:
+        has_song_results = True
+
+        songs_table = song_results.find_next('table')
+        for item in songs_table.find_all('a')[:5]:
+            link = item['href']
+            song_links.append(link)
+
+            song = item.find_next('b').text
+            song_songs.append(song.replace('"', ""))
+
+            artist = item.find_next('b').find_next('b').text
+            song_artists.append(artist)
+
+    if lyrics_results:
+        has_lyrics_results = True
+
+        lyrics_table = lyrics_results.find_next('table')
+
+        num_results = int(soup.find(
+            'div', {'class': 'panel-heading'}).find('small').text.split('of ')[1].split(' found')[0])
+
+        # if num_results >= 20:
+        #     lyrics_table = lyrics_table.find_all('a')[:5]
+        lyrics_table = lyrics_table.find_all('a')[:5]
+        for item in lyrics_table:
+            link = item['href']
+            lyrics_links.append(link)
+
+            song = item.findChild()
+            if song:
+                lyrics_songs.append(song.text.replace('"', ""))
+
+            artist = song.find_next('b')
+            if artist:
+                lyrics_artists.append(artist.text)
+
+    song_results = list(zip(song_songs, song_artists, song_links))
+    lyrics_results = list(zip(lyrics_songs, lyrics_artists, lyrics_links))
+
+    if not has_song_results and not has_lyrics_results:
+        return "Could not find any results.", {}
+
+    choices = {}
+    message = ""
+    lyrics_start_index = 1
+    if has_song_results:
+        message += "Song results:\n"
+        lyrics_start_index += len(song_results)
+
+        for i, result in enumerate(song_results, 1):
+            message += f"{i}. {result[1]} - {result[0]}\n"
+            choices[i] = result[2]
+
+        message += "\n"
+
+    if has_lyrics_results:
+        message += "Lyrics results:\n"
+
+        for i, result in enumerate(lyrics_results, lyrics_start_index):
+            message += f"{i}. {result[1]} - {result[0]}\n"
+            choices[i] = result[2]
+
+        message += "\n"
+
+    message += "Choose a song by typing its number."
+    return message, choices  # string, dict
+
+
+if __name__ == "__main__":
+    result = scrape_top_five("jungle")
+    print(result)
